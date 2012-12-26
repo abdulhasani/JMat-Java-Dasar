d46709d8c2ee76fa1d68ab4e7340ec6aaa8ae9ab
joker <joker@joker-Aspire-4741>
2012-12-26 14:43:19 +0700

    :P

---
+++ Java Dasar/String/Belajar String/src/san/jmat/belajar/string/BelajarString.java
@@ -0,0 +1,28 @@
+/*
+ * |----------------JMat (Java Master)-------------------------------------|
+ * |Setiap Manusia Yang Menata Hidupnya adalah Programmer                  |
+ * |Hak Cipta Hanya Milik Allah SWT, Hamba-Nya Hanya Memanfaatkan.         |
+ * |Manusia Tidak Bisa Mewujudkan Apa-apa, Manusia Hanya Bisa Merencanakan.| 
+ * |----------------JMat (Java Master)-------------------------------------| 
+ */
+package san.jmat.belajar.string;
+
+/**
+ *
+ * @author joker
+ */
+public class BelajarString {
+    
+    public static void main(String  [] args){
+        String nama;
+        nama = "Abdul Kadir Hasani";
+        String fakultas;
+        fakultas = "Teknik";
+        String prodi;
+        prodi = "Informatika";
+        System.out.println("Nama\t\t : "+nama);
+        System.out.println("Fakultas\t : "+fakultas);
+        System.out.println("Prodi\t\t : "+prodi);
+    }
+    
+}
