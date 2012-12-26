90b03aaa4e014e13cc9e1df6e717fecd2c830e55
joker <joker@joker-Aspire-4741>
2012-12-27 01:04:04 +0700

    :P

---
+++ Java Dasar/Deklarasi Varibel/DeklarasiVariabel_1/src/deklarasivariabel_/DeklarasiVariabel1.java
@@ -0,0 +1,57 @@
+package deklarasivariabel_;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joker
+ */
+public class DeklarasiVariabel1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Varibel dan Insialisasi Cara 1
+
+        //Proses Deklarasi varibel
+        byte angka;
+        short nilai;
+        int uang;
+        long hutang;
+        char karakter;
+        float bilanganBerkoma;
+        double angkaBerkom;
+        boolean statusSingle;
+        boolean belumMenikah;
+        //Proses Inisialisasi Varibel
+
+        angka = 12;
+        nilai = 300;
+        uang = 1000000;
+        hutang = 2_000_000l;
+        karakter = 'S';
+        bilanganBerkoma=45.34499f;
+        angkaBerkom=45.999999;
+        statusSingle = false;
+        belumMenikah=true;
+        
+        //Cetak Isi Variabel 
+        System.out.println(angka);
+        System.out.println(nilai);
+        System.out.println(uang);
+        System.out.println(hutang);
+        System.out.println(karakter);
+        System.out.println(bilanganBerkoma);
+        System.out.println(angkaBerkom);
+        System.out.println(statusSingle);
+        System.out.println(belumMenikah);
+        
+        System.out.println("Bilangan Float "+bilanganBerkoma);
+        System.out.println("Bilangan Double "+angkaBerkom);
+        
+    }
+}
