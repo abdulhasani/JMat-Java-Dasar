b39776e6d92e2a2deeb3796eb3124ab335d65f60
Bang JOker <joker@joker-Aspire-4741.(none)>
2012-12-26 11:09:52 +0700

    Belajar Operator Aritmatika

---
+++ Java Dasar/Operator/Operator Aritmatika/Aritmatika TakLangsung/src/aritmatika/taklangsung/AritmatikaTakLangsung.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika.taklangsung;
+
+/**
+ *
+ * @author joker
+ */
+public class AritmatikaTakLangsung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a;
+        int b;
+        int h;
+        a = 10;
+        b = 10;
+        h = a + b;
+        
+        System.out.println("Penjumlahan "+a+" + "+b+" = "+h);
+
+    }
+}
