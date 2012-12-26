0405391cbc5b9f231897a5134d1b2391dba42980
joker <joker@joker-Aspire-4741>
2012-12-26 09:22:08 +0700

    Cara mendeklarasiakan variabel ketiga

---
+++ Java Dasar/Deklarasi Varibel/Deklarasi Variabel_3/src/deklarasi/variabel_3/DeklarasiVariabel_3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deklarasi.variabel_3;
+
+/**
+ *
+ * @author joker
+ */
+public class DeklarasiVariabel_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char a = 'A', b = 'B', c = 'C';
+        int nilaiTugas = 100, nilaiUAS = 80, nilaiUTS = 60;
+
+        System.out.println("Nialai Huruf = " + a);
+        System.out.println("Nialai Huruf = " + b);
+        System.out.println("Nialai Huruf = " + c);
+        System.out.println();
+        System.out.println("Nilai Angka Tugas = " + nilaiTugas);
+        System.out.println("Nilai Angka UAS = "+nilaiUAS);
+        System.out.println("Nilai Angka UTS ="+nilaiUTS);
+    }
+}
