c88f3b360c1aee83e48d8039008a3b25fcb84b8b
joker <joker@joker-Aspire-4741>
2012-12-26 14:41:20 +0700

    :P

---
+++ Java Dasar/Operator/Operator Unery/Operator Unery/src/javaapplication121/JavaApplication121.java
@@ -0,0 +1,28 @@
+/*
+ * |----------------JMat (Java Master)-------------------------------------|
+ * |Setiap Manusia Yang Menata Hidupnya adalah Programmer                  |
+ * |Hak Cipta Hanya Milik Allah SWT, Hamba-Nya Hanya Memanfaatkan.         |
+ * |Manusia Tidak Bisa Mewujudkan Apa-apa, Manusia Hanya Bisa Merencanakan.| 
+ * |----------------JMat (Java Master)-------------------------------------| 
+ */
+package javaapplication121;
+
+/**
+ *
+ * @author joker
+ */
+public class JavaApplication121 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        int angka = 2;
+        angka += 1;
+        // angka++;
+        System.out.println(angka);
+
+    }
+}
